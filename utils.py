e0c8a05fc566bd176e17759a3a5be0955ce6dfef
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-06-04 18:53:20 +0200

    reformat file

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
+"""math functions"""
 # przykladowe funkcje utils . py
-def add ( a : int , b : int ) - > int :
+def add ( a : int , b : int ) -> int:
+    """add function"""
     return a + b
-def subtract ( a : int , b : int ) - > int :
+def subtract ( a : int , b : int ) -> int:
+    """substract function"""
     return a - b
-def multiply ( a : int , b : int ) - > int :
+def multiply ( a : int , b : int ) -> int:
+    """multiply function"""
     return a * b
-def divide ( a : int , b : int ) - > float :
+def divide ( a : int , b : int ) -> float:
+    """div function"""
     return a / b
